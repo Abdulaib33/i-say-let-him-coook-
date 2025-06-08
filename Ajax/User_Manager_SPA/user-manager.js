59348a4ca6c15a887e54c0ca76499db1e8163870
Abdoulaye <Abdulaibayo@outlook.fr>
2025-06-08 14:58:32 +0200

    i have no idea why the 'Add a New User (POST)' dont work, i think the problem is with our api we will check it later

--- Ajax/User_Manager_SPA/user-manager.js
+++ Ajax/User_Manager_SPA/user-manager.js
@@ -38,8 +38,57 @@ function displayUsers(users) {
         div.innerHTML = `
             <strong>${user.first_name} ${user.last_name}</strong> (${user.email})
             <button onclick="deleteUser(${user.id})">Delete</button>
-        `
+            <img src="${user.avatar}" alt="">
+        `;
         list.appendChild(div)
     })
+
+}
+
+
+
+
+
+
+// Add a New User (POST)
+
+
+document.getElementById('add-user-form').addEventListener('submit', async (e) => {
+    e.preventDefault(); // prevent page reload
+})
+
+const nameInput = document.getElementById('name')
+const jobInput = document.getElementById('job')
+const name1 = nameInput.value.trim()
+const job = jobInput.value.trim()
+
+
+if (!name1 || !job) return alert('Please enter both name and job');
+
+
+try {
+
+    const response = await fetch("https://reqres.in/api/users", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({name1, job})
+    })
+
+    if (!response.ok) throw new Error('Failed to add user')
+
+    const data = await response.json()
+    alert(`User added: ${data.name}, ${data.job}`)
+
+    // Optionally clear input fields
+    nameInput.value = "";
+    jobInput.value = "";
+
+
+    // Reload user list
+    loadUsers();
+} catch (error) {
+
+    console.error('Error adding user:', error.message);
+    alert('Something went wrong when adding user.')
 
 }
