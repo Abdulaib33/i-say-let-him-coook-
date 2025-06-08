24e4662e48b30647d23dfa40cee90915079ccba0
Abdoulaye <Abdulaibayo@outlook.fr>
2025-06-08 17:34:15 +0200

    config connexion file done ✅ in our php mvc procedural with ajax vuejs(CDN)

--- Ajax/user-manager/backend/config/database.php
+++ Ajax/user-manager/backend/config/database.php
@@ -1,3 +1,20 @@
 <?php 
 // config/database.php
 
+
+$host = 'localhost';
+$dbname = "user_manager";
+$username = "root";
+$password = "becode";
+
+try {
+
+    $pdoConnexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdoConnexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (error) {
+
+    http_response_code(5000);
+    echo json_encode(['error' => 'Database connection failed :' . $e->getMessage()]);
+    exit();
+
+}
