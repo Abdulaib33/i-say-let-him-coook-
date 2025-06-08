b9c4b0bd3c63ba4ea7f9507fbd18bc72e7a6ba41
Abdoulaye <Abdulaibayo@outlook.fr>
2025-06-08 14:11:50 +0200

    the user are not usually load in the 'loadUsers()'

---
+++ Ajax/User_Manager_SPA/user-manager.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadUsers()
+})
+
+
+async function loadUsers() {
+    try {
+
+        const response = await fetch("https://reqres.in/api/users?page=1")
+        if (!response.ok) throw new Error('Failed to fetch users')
+
+        const data = await response.json()
+        displayUsers(data.data)
+
+    } catch (error) {
+
+        console.error('Error loading users', error.message)
+        document.getElementById('user-list').textContent = "Error loading users."
+
+    }
+}
+
+
+function displayUsers(users) {
+
+    const list = document.getElementById('user-list')
+    list.innerHTML = ""; // Clear previous content
+
+    users.forEach(user => {
+        const div = document.createElement('div')
+        div.className = 'user'
+        div.innerHTML = `
+            <strong>${user.first_name} ${user.last_name}</strong> (${user.email})
+            <button onclick="deleteUser(${user.id})">Delete</button>
+        `
+        list.appendChild(div)
+    })
+
+}
