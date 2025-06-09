7d7cb5080e3efbb406daddc24480f67f9737885a
Abdoulaye <Abdulaibayo@outlook.fr>
2025-06-09 16:34:38 +0200

    taskController done ✅

--- Ajax/task-tracker/backend/controllers/taskController.php
+++ Ajax/task-tracker/backend/controllers/taskController.php
@@ -26,3 +26,27 @@ function handlePost($pdoConnexion) {
     echo json_encode(['message' => 'Task created', 'id' => $id]);
 
 }
+
+
+
+function handlePut($pdoConnexion, $id) {
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    if (!isset($data['title']) || !isset($data['status'])) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Missing title or status']);
+        return;
+    }
+
+    $success = updateTask($pdoConnexion, $id, $data);
+    echo json_encode(['message' => $success ? 'Task updated' : 'Update failed']);
+}
+
+
+
+function handleDelete($pdoConnexion, $id) {
+
+    $success = deleteTask($pdoConnexion, $id);
+    echo json_encode(['message' => $success ? 'task delete' : 'failed delete']);
+
+}
