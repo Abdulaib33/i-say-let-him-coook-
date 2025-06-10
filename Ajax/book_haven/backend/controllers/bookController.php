3cc8562340815dc8943ded27a2e4ae56ff759635
Abdoulaye <Abdulaibayo@outlook.fr>
2025-06-10 20:42:00 +0200

    bookController is done ✅, hope there's no problem or syntax error

--- Ajax/book_haven/backend/controllers/bookController.php
+++ Ajax/book_haven/backend/controllers/bookController.php
@@ -10,3 +10,45 @@ function handleGet($connexion) {
     $books = getAllBooks($connexion);
     echo json_encode($books);
 }
+
+
+function handlePost($connexion) {
+
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    if (!isset($data['title'], $data['author'])) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Missing data']);
+        return;
+    }   
+
+    addBook($connexion, $data['title'], $data['authro']);
+    http_response_code(201);
+    echo json_encode(['message' => 'Book added']);
+
+}
+
+
+
+function handlePut($connexion, $id) {
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    if (!isset($data['title'], $data['author'])) {
+        http_response_code(400);
+        echo json_encode(['error' => 'missing data']);
+        return;
+    }
+
+    updateBook($connexion, $id, $data['title'], $data['author']);
+    http_response_code(201);
+    echo json_encode(['message' => 'Book Updated']);
+}
+
+
+
+function handleDelete($connexion, $id) {
+
+    deleteBook($connexion, $id);
+    echo json_encode(['message' => 'book deleted']);
+
+}
