556d77ede26d3516de0d64d7fa7a783bed1a3382
Abdoulaye <Abdulaibayo@outlook.fr>
2025-06-13 03:00:37 +0200

    🔁 SECTION 2: ARRAY METHODS MASTERY 🇧🇪 part 6 DONE ✅

--- Object & Array, Allah is the Greatest/Section2.js
+++ Object & Array, Allah is the Greatest/Section2.js
@@ -9,3 +9,25 @@ const prices = [100, 200, 3000]
 
 const pricesWithTax = prices.map(price => price * 1.1)
 console.log(pricesWithTax)
+
+
+
+
+// 6. filter() challenge:
+    // From users array, remove all users under 18.
+        // const users = [
+        //     { name: 'John', age: 15 },
+        //     { name: 'Jane', age: 22 },
+        //     { name: 'Bob', age: 17 },
+        // ];
+
+
+const users = [
+    {name: 'John', age: 15},
+    {name: 'Jane', age: 22},
+    {name: 'Bob', age: 17},
+    {name: 'Abdoulaye', age: 22}
+]
+
+const filteredUsers = users.filter(user => user.age >= 18)
+console.log(filteredUsers)
