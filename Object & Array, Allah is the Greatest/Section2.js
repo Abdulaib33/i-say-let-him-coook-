f129d61dbba400be7313a16baac64843545ff06c
Abdoulaye <Abdulaibayo@outlook.fr>
2025-06-13 02:55:58 +0200

    🔁 SECTION 2: ARRAY METHODS MASTERY 🇧🇪 part 5 DONE ✅

--- Object & Array, Allah is the Greatest/Section2.js
+++ Object & Array, Allah is the Greatest/Section2.js
@@ -1,1 +1,11 @@
-console.log("COUCOU")
+// 🔁 SECTION 2: ARRAY METHODS MASTERY
+
+// 5. map() exercise:
+    // Given prices: [100, 200, 300], return array with 10% tax added.
+    // => [110, 220, 330]
+
+
+const prices = [100, 200, 3000]
+
+const pricesWithTax = prices.map(price => price * 1.1)
+console.log(pricesWithTax)
