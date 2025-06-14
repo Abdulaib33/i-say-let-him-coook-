40b32bd9023d982e5c233e7f958c0f5772387d7e
Abdoulaye <Abdulaibayo@outlook.fr>
2025-06-14 17:37:42 +0200

    🔄 SECTION 5: ADVANCED PATTERNS 🇧🇪 part 16 DONE ✅

--- Object & Array, Allah is the Greatest/Section5.js
+++ Object & Array, Allah is the Greatest/Section5.js
@@ -36,3 +36,37 @@ const {profile: {name, location: {city, zip}}} = user
 console.log(name)
 console.log(city)
 console.log(zip)
+
+
+
+
+
+// 16. Use rest/spread in objects:
+
+    // Exclude a property: const { password, ...rest } = user
+
+    // Merge default config and user config:
+
+    // const defaultConfig = { theme: 'dark', lang: 'en' };
+
+    // const userConfig = { lang: 'fr' };
+
+        // Result: { theme: 'dark', lang: 'fr' }
+
+
+
+const user1 = {
+    username: "JohnDoe",
+    email: "john@example.com",
+    password: "secret123"
+}
+
+const {password, ...rest} = user1
+
+
+const defaultConfig = {theme: "dark", lang: "en"}
+const userConfig = {lang: "fr"}
+const mergedConfig = { ...defaultConfig, ...userConfig}
+
+console.log(rest) // {username: 'JohnDoe', email: 'john@example.com'}
+console.log(mergedConfig) // {theme: 'dark', lang: 'fr'}
